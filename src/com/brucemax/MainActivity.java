598793e6a4bb12d1c8b894b758e8e56d9a372853
brucemax <bruce.max88@gmail.com>
2012-06-25 12:45:59 +0300

    В главном окне реализованы две вкладки (activities)

--- src/com/brucemax/MainActivity.java
+++ src/com/brucemax/MainActivity.java
@@ -1,13 +1,28 @@
 package com.brucemax;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        TabHost tabHost = getTabHost();
+        
+        TabHost.TabSpec tabSpec;
+        tabSpec = tabHost.newTabSpec("tag1");
+        tabSpec.setIndicator("¬кладка1");
+        tabSpec.setContent(new Intent (this,OneActivity.class));
+        tabHost.addTab(tabSpec);
+        
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setIndicator("¬кладка 2");
+        tabSpec.setContent(new Intent(this, TwoActivity.class));
+        tabHost.addTab(tabSpec);
     }
 }
---
+++ src/com/brucemax/TwoActivity.java
@@ -0,0 +1,13 @@
+package com.brucemax;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TwoActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.two);
+	}
+
+}

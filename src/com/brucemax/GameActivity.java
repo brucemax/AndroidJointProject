5b00c9cae9ee3abfdde5afefe6172a9cca008c88
brucemax <bruce.max88@gmail.com>
2012-06-26 16:43:00 +0300

    Создано игровое активити и реализован переход к нему по нажатию на кнопку.

---
+++ src/com/brucemax/GameActivity.java
@@ -0,0 +1,17 @@
+package com.brucemax;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class GameActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.game_activity);
+		Log.d("myLogs", "GameActivity create");
+		
+		
+		
+	}
+
+}
--- src/com/brucemax/ThreeActivity.java
+++ src/com/brucemax/ThreeActivity.java
@@ -1,13 +1,35 @@
 package com.brucemax;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class ThreeActivity extends Activity {
-
+public class ThreeActivity extends Activity implements OnClickListener{
+	Button btnGo;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.three);
+		btnGo = (Button) findViewById(R.id.button1);
+		btnGo.setOnClickListener(this);
+		Log.d("myLogs", "create ThreeActivity");
+		
+	}
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		 
+		Log.d("myLogs", "onclick in ThreeActivity");
+			Intent intent = new Intent(this, GameActivity.class);
+			
+			startActivity(intent);
+		
+		
+		
 	}
 
 }
